b96caf107fe4acc008fc848f7b82ed005fce76f4
alu0100700459 <alu0100700459@ull.edu.es>
2015-12-14 23:02:56 +0000

    Implementación de toda la navegación de la aplicación y creación de plantillas para todas las vistas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,7 +10,36 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('MapController', function ($scope, $ionicModal, $state) {
+.controller('MenuController', function ($scope) {
+  $scope.filter = function () {
+    var prefix = "";
+
+    function queryStr (b, value) {
+      var str = "";
+
+      if (b === true) {
+        str = prefix + "Valoración = '" + value + "'";
+        prefix = " OR ";
+      }
+
+      return str;
+    };
+
+    var query = queryStr($scope.excelCheck, 3) +
+                queryStr($scope.mbCheck,    2) +
+                queryStr($scope.bCheck,     1) +
+                queryStr($scope.rCheck,     0) +
+                queryStr($scope.mCheck,    -1) +
+                queryStr($scope.mmCheck,   -2) +
+                queryStr($scope.pCheck,    -3);
+
+    responsePoints.setDefinitionExpression(query);
+    map.addLayers([responsePoints]);
+    map.setExtent(map.extent);
+  };
+})
+
+.controller('MapController', function ($scope, $state, $ionicModal) {
   // TODO: Añadir a $scope.userImage la imagen del usuario actual (si inició sesión)
   loadMap();
 
@@ -47,6 +76,19 @@ angular.module('starter.controllers', [])
   $scope.loggedIn = false;
 })
 
-.controller('AddController', function ($scope) {
+.controller('AddController', function ($scope, $state, $ionicHistory) {
   // TODO: Añadir a $scope.userImage la imagen del usuario actual (si inició sesión)
+  $scope.postForm = function () {
+    // TODO Enviar todos los datos del formulario y la posición seleccionada en el mapa al servidor
+    // TODO Posiblemente mostrar un popup cuando el envío haya terminado para avisar al usuario
+
+    // Deshabilitar el volver atrás y cambiar a la pantalla principal
+    $ionicHistory.nextViewOptions({
+      disableBack: true,
+      historyRoot: true
+    });
+
+    $state.go('map');
+  };
+
 });
--- www/js/map.js
+++ www/js/map.js
@@ -1,6 +1,6 @@
 var map;
  var responsePoints;
- 
+
 function loadMap () {
 require([
   "esri/map",
@@ -173,60 +173,3 @@ function initEditor (
 
   myEditor.startup();
 }
-function filter() {
-   var selected = "";
-   var excel = document.getElementById('excelcheck').checked;
-    if (excel == true){
-	 selected += "Valoración = '3' ";
-	}
-    var mb = document.getElementById('mbcheck').checked;
-    if (mb == true){
-	 if (selected != "")
-		selected += "OR Valoración = '2' ";
-	 else
-		selected += "Valoración = '2' ";
-	}
-	var b = document.getElementById('bcheck').checked;
-    if (b == true){
-	 if (selected != "")
-		selected += "OR Valoración = '1' ";
-	 else
-		selected += "Valoración = '1' ";
-	}
-	
-	var r = document.getElementById('rcheck').checked;
-    if (r == true){
-	 if (selected != "")
-		selected += "OR Valoración = '0' ";
-	 else
-		selected += "Valoración = '0' ";
-	}
-	
-	var m = document.getElementById('mcheck').checked;
-    if (m == true){
-	 if (selected != "")
-		selected += "OR Valoración = '-1' ";
-	 else
-		selected += "Valoración = '-1' ";
-	}
-	
-	var mm = document.getElementById('mmcheck').checked;
-    if (mm == true){
-	 if (selected != "")
-		selected += "OR Valoración = '-2' ";
-	 else
-		selected += "Valoración = '-2' ";
-	}
-
-	var p = document.getElementById('pcheck').checked;
-    if (p == true){
-	 if (selected != "")
-		selected += "OR Valoración = '-3' ";
-	 else
-		selected += "Valoración = '-3' ";
-	}
-
-	responsePoints.setDefinitionExpression(selected);
-	map.addLayers([responsePoints]);
-    map.setExtent(map.extent);
-	}
